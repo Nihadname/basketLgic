913df6b02a09362201c2853fa4cf7beaf329540d
Nihadname <nihadcoding@gmail.com>
2024-03-18 22:27:51 +0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+let allAddingBasketButtons = document.querySelectorAll(".btn-primary");
+let Basketcount=document.querySelector("#count");
+
+allAddingBasketButtons.forEach(item => {
+    item.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let productsArr=[];
+        let productId=this.parentElement.getAttribute("data-id");
+        if(localStorage.getItem("basket")==null){
+            localStorage.setItem("basket",JSON.stringify([]));
+        }else{
+            productsArr=JSON.parse(localStorage.getItem("basket"))
+        }
+        let existProduct=productsArr.find(p=>p.id==productId);
+        if(existProduct){
+            existProduct.count++;
+        }else{ 
+            let products={
+            id:productId,
+            name:this.parentElement.firstElementChild.innerText,
+            desc:this.previousElementSibling.previousElementSibling.innerText,
+            price:this.previousElementSibling.innerText,
+            count:1
+        }
+        productsArr.push(products);
+        }
+        localStorage.setItem("basket",JSON.stringify(productsArr));
+
+        calculationBasketCount();
+
+    })
+})
+
+function calculationBasketCount(){
+    let basket=localStorage.getItem("basket");
+    let length;
+    if(basket){
+        length=JSON.parse(basket).length;
+        Basketcount.innerText=length;
+    }
+};
+calculationBasketCount();

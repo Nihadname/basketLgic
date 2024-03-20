a0bdb8477abd3806f95607b76df2038f71980efd
Nihadname <nihadcoding@gmail.com>
2024-03-20 15:08:45 +0400

    new update

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -26,6 +26,21 @@ getBasket().forEach(products => {
      tdCount.innerText=products.count;
      let TdRemove=document.createElement("td");
      TdRemove.innerHTML=`<i class="fa-solid fa-trash"></i>`;
+     let increaseSpan = document.createElement("span");
+let decreaseSpan = document.createElement("span");
+increaseSpan.innerHTML = "+";
+decreaseSpan.innerHTML = "-";
+
+// Add click event listeners to each span
+increaseSpan.addEventListener("click",function(){
+    products.count++;
+    tdCount.innerText=products.count;
+    tdPrice.innerText=(products.price *products.count)+"$";
+})
+
+// Append the spans to the 'tdCount' cell
+tdCount.appendChild(decreaseSpan);
+tdCount.appendChild(increaseSpan);
      tr.append(tdImage,tdName,tdPrice,tdCount,TdRemove)
      let table=document.querySelector(".table");
      table.lastElementChild.append(tr);
